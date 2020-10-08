7f4db9fa28390e69f8b4652183ed4c931d65b8e4
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-08 10:21:10 +0530

    MasterTicTac.java

--- MasterTicTac.java
+++ MasterTicTac.java
@@ -1,4 +1,24 @@
+package com.capgem.TicTacToeClass;
 
-public class MasterTicTac {
 
+//UC1 Test case
+public class TicTacToeGame{
+	public static final char NULL=' ';
+	private static char[] ticTacBoard;
+
+public TicTacToeGame( ) {
+	super();
+	ticTacBoard = new char [10];
+}
+
+public static void createBoard() {
+	for (int i=1; i<ticTacBoard.length;i++) {
+		ticTacBoard[i] = NULL;
+	}
+}
+
+public static void main (String[] args) {
+	TicTacToeGame ticTacObject = null;
+	ticTacObject.createBoard();
+  }
 }
